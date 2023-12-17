ea52a26c10c950c6a3e96d3d1a8946981425f0f3
tjektmf <seul123ds@gmail.com>
2023-12-17 23:21:45 +0900

    내용추가

--- js/resource/js16-2.js
+++ js/resource/js16-2.js
@@ -3,12 +3,16 @@ const lottoNum = [];
 const lottoCount = [];
 
 // LottoGame 함수는 그냥 한번에 두가지 값을 넣는 정도의 의미?
+// 이 함수 자체에 값을 넣는게 아니라 변수에 값을 넣을때 이 함수를 통해 값을 넣는거임
+// 그래서 lottoCount[i] = new LottoGame(i + 1, 0);
+// 이 부분에서 lottoCount는 LottoGame 함수가 가진 num과 count 두가지 값을 가짐
+// num값을 꺼낼때는 lottoCount[i].num 을 사용하고 count값을 꺼낼때는 lottoCount[i].count를 사용함
 function LottoGame(num, count) {
     this.num = num;
     this.count = count;
 }
 
-// 여기서 두 값을 모두 숫자로 초기화함
+// 여기서 두 값을 모두 숫자로 초기화함 안하면 NaN 뜨던데
 for (let i = 0; i < 45; i++) {
     lottoCount[i] = new LottoGame(i + 1, 0);
 }
@@ -27,6 +31,9 @@ for (let i = 0; i < 1000; i++) {
 }
 
 // 이제 lottoCount.count를 내림차순으로 정렬해야함 sort를 사용
+// 두 값의 위치를 바꿀 때 양수를 리턴 양수는 긍정의 의미니까
+// 자리바꿈? ㅇㅇ > 양수라고 생각하면 될듯
+// 양수를 리턴하면 두 값의 위치를 바꾸고, 음수를 리턴하면 위치를 바꾸지 않음
 const descendLotto = lottoCount.sort((a, b) => {
     if (a.count > b.count) {
         return -1;
