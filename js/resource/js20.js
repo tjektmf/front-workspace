f2dc92292c314ea1759f5f0f766288eddf0d4467
tjektmf <seul123ds@gmail.com>
2023-12-18 21:42:39 +0900

    집에서 끝냄

--- js/resource/js20.js
+++ js/resource/js20.js
@@ -34,22 +34,27 @@ const popbutton = document.querySelector('#pop');
 shiftbutton.addEventListener('click', (e) => {
     const newDiv2 = document.createElement('div');
     const newDiv3 = document.createElement('div');
+    const newDiv4 = document.createElement('div');
     const newText2 = document.createTextNode('Abc');
     const newText3 = document.createTextNode(counting);
+
     newDiv2.classList.add('material-symbols-outlined');
+    newDiv2.style.display = 'grid';
+    newDiv2.style.gridTemplateColumns = '1fr 1fr';
     newDiv2.style.fontSize = '30px';
     newDiv2.style.color = 'red';
     newDiv2.style.padding = '5px';
+    newDiv2.style.width = '50px';
+    
 
-    newDiv3.style.verticalAlign = 'top';
-    newDiv3.style.fontSize = '0.4em';
-    newDiv3.style.paddingLeft = '20px';
+    newDiv4.style.verticalAlign = 'top';
+    newDiv4.style.fontSize = '0.3em';
     newDiv3.style.color = 'red';
-    newDiv2.appendChild(newText2);
     newDiv2.appendChild(newDiv3);
-    newDiv3.appendChild(newText3);
+    newDiv3.appendChild(newText2);
+    newDiv2.appendChild(newDiv4);
+    newDiv4.appendChild(newText3);
     out.insertBefore(newDiv2, out.firstChild);
-  //  out.insertBefore(newDiv3, out.firstChild);
     counting++;
 
 });
@@ -58,7 +63,7 @@ unshiftbutton.addEventListener('click', (e) => {
     out.removeChild(out.firstElementChild);
 });
 
-popbutton.addEventListener('click', (e)=>{
+popbutton.addEventListener('click', (e) => {
     out.removeChild(out.lastChild);
 
 });
